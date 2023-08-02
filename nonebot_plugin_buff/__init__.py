3425b82d9ebd4a1a02be15ff507d31634f8c7825
Sydrr0 <129657153+Sydrr0@users.noreply.github.com>
2023-08-02 15:01:03 +0800

    __init__.py

--- nonebot_plugin_buff/__init__.py
+++ nonebot_plugin_buff/__init__.py
@@ -8,9 +8,9 @@ __plugin_meta__ = PluginMetadata(
     # 发布必填，当前有效类型有：`library`（为其他插件编写提供功能），`application`（向机器人用户提供功能）。
     homepage="https://github.com/Sydrr0/nonebot-plugin-buff",
     # 发布必填。
-    config=Config,
+    # config=Config,
     # 插件配置项类，如无需配置可不填写。
-    supported_adapters={"~onebot.v11"},
+    # supported_adapters=None
     # 支持的适配器集合，其中 `~` 在此处代表前缀 `nonebot.adapters.`，其余适配器亦按此格式填写。
     # 若插件可以保证兼容所有适配器（即仅使用基本适配器功能）可不填写，否则应该列出插件支持的适配器。
 )
