bd3d976a4a43cab87d83ac9d1e0aa8e115711f77
SydrrO <129657153+SydrrO@users.noreply.github.com>
2023-09-05 16:32:47 +0800

    Update __init__.py

--- nonebot_plugin_csornament/__init__.py
+++ nonebot_plugin_csornament/__init__.py
@@ -1,22 +1,22 @@
 from nonebot.plugin import PluginMetadata
+
 __plugin_meta__ = PluginMetadata(
     name="CSGO饰品查询机器人",
     description="一款模拟查找饰品价格的机器人",
     usage="输入 查询/搜索/查 xxx 按照机器人的提示，将会获取到目前市场上最低的饰品价格",
     type="application",
     # 发布必填，当前有效类型有：`library`（为其他插件编写提供功能），`application`（向机器人用户提供功能）。
-    homepage="https://github.com/Sydrr0/nonebot-plugin-csornament",
+    homepage="https://githubfast.com/Sydrr0/nonebot-plugin-csornament",
     # 发布必填。
 )
+
 # nonebot2 的函数导入
 from nonebot.matcher import Matcher
 from nonebot.plugin import on_command
-from nonebot.params import ArgPlainText # 提取消息内的字符串的方法
+from nonebot.params import ArgPlainText  # 提取消息内的字符串的方法
 from nonebot.adapters import Message
-from nonebot.params import CommandArg 
-from nonebot.typing import T_State   # 继承上一个函数的方法
-
-
+from nonebot.params import CommandArg
+from nonebot.typing import T_State  # 继承上一个函数的方法
 
 # 后端爬虫所需要的插件
 import csv
@@ -24,7 +24,7 @@ import httpx
 import re
 import os
 from bs4 import BeautifulSoup
-import lxml #为了防止解析时bs4拉不出屎的毛病，导入lxml解析器
+import lxml  # 为了防止解析时bs4拉不出屎的毛病，导入lxml解析器
 
 # 创建本地文件夹，用于存放数据
 save_path = "./search_data"
@@ -33,19 +33,16 @@ if not os.path.exists(save_path):
 else:
     pass
 
-
-
 search = on_command("查询", aliases={"搜索", "查"}, priority=10, block=True)
-
-
-
 
 
 def get_correct_lists(keywords_str) -> list:
     with open("search_data/data.csv", "r", encoding="utf-8") as file:
+        # 打开数据集文件
         data_lines = csv.reader(file)
-
+        
         if any(char.isalpha() for char in keywords_str):
+            # 判断是否含有字母，如果有字母就遍历含有字母的名称
             keywords = [keyword.lower() for keyword in keywords_str]
         else:
             keywords = keywords_str.lower()
@@ -57,57 +54,53 @@ def get_correct_lists(keywords_str) -> list:
             lowercase_elements = elements[1].lower()
 
             if all(keyword in lowercase_elements for keyword in keywords):
+                # 判断是否含有所有目标关键词，含有便返回list
                 numindex = int(elements[0])
                 content = elements[1]
                 num_list.append(numindex)
                 ob_name_list.append(content)
 
         return num_list, ob_name_list
-    
 
 
-
-
-def sending_txt(name_index):   # 传入两个列表，第一个是符合的商品编码。第二个是名称
-    name_result_list = []    # 得到一个用以下 for 循环的结果，需要逐行输出
-    for index in range(0,len(name_index)):
+def sending_txt(name_index):  # 传入两个列表，第一个是符合的商品编码。第二个是名称
+    name_result_list = []  # 得到一个用以下 for 循环的结果，需要逐行输出
+    for index in range(0, len(name_index)):
         # 把每一行的内容加入到name_resulr_list里面
-        name_result_list.append('%i: %s'% (index, name_index[index]))          
-    # 用换行符把列表里面每一个元素 join 成一个对象
+        name_result_list.append('%i: %s' % (index, name_index[index]))
+        # 用换行符把列表里面每一个元素 join 成一个对象
     result = '\n'.join(str(item) for item in name_result_list)
     # 去除最后一行的换行符
     result = result.rstrip('\n')
     return result
 
 
-
-async def get_miniprice(num,obtype): # num是获取到的商品index, ob_type是指崭新出厂之类的
-#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ob_type 还未表达，需使用正则表达式提取 !!!!!
-    base_url ='https://buff.163.com/goods/'
+async def get_miniprice(num, obtype):  # num是获取到的商品index, ob_type是指崭新出厂之类的
+    # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ob_type 还未表达，需使用正则表达式提取 !!!!!
+    base_url = 'https://buff.163.com/goods/'
     extra_url = str(num)
     url = base_url + extra_url
-    async with httpx.AsyncClient() as client: # 异步 httpx 为了防止事件卡死
+    async with httpx.AsyncClient() as client:  # 异步 httpx 为了防止事件卡死
         response = await client.get(url)
         page_url = response.text
         data_getten = BeautifulSoup(page_url, "lxml")
         mini_price_html = str(data_getten.find_all("div", class_="scope-btns"))
 
-    pattern = r'{ob_type}.*?data\-price\="([^"]+)"'.format(ob_type = obtype) 
+    pattern = r'{ob_type}.*?data\-price\="([^"]+)"'.format(ob_type=obtype)
     # 上面的正则表达式表示 ob_type 后面的第一个 data-price='' 中的值
     match = re.search(pattern, mini_price_html)
     if match:
-        return match.group(1) # 查找第一个符合的值
+        return match.group(1)  # 查找第一个符合的值
     else:
         return None
-
-
 
 
 @search.handle()  # 这是为了接收 查询xxx 中的xxx
 async def handle_function(matcher: Matcher, args: Message = CommandArg()):
     if args.extract_plain_text():
         matcher.set_arg("name", args)  # 获取的xxx
-    
+
+
 @search.got("name", prompt="请输入饰品名称")
 async def got_name(state: T_State, name: str = ArgPlainText()):
     name_input = f"{name}"  # 用户输入的饰品名称
@@ -115,7 +108,8 @@ async def got_name(state: T_State, name: str = ArgPlainText()):
         num_list = get_correct_lists(name_input)[0]  # 获取序号的列表
         obname_list = get_correct_lists(name_input)[1]  # 获取物品名称的列表
     except:
-        await search.finish('data.csv未放入指定文件夹\nreadme是不是没看!!!\nhttps://github.com/Sydrro/nonebot-plugin-csornament/tree/main')
+        await search.finish(
+            'data.csv未放入指定文件夹\nreadme是不是没看!!!\nhttps://githubfast.com/Sydrro/nonebot-plugin-csornament/tree/main')
     asking_txt = sending_txt(name_index=obname_list)
     state['num_list'] = num_list  # 保存list到下一个函数内使用
     state['name_list'] = obname_list
@@ -125,6 +119,7 @@ async def got_name(state: T_State, name: str = ArgPlainText()):
         await search.finish('数据库中暂时未收录该饰品~')
     else:
         await search.send(asking_txt)
+
 
 # 正则表达式获取最后一个括号内的内容，就是obtype
 
@@ -138,7 +133,7 @@ async def got_num(state: T_State, num_index: str = ArgPlainText()):
         else:
             pass
     except:
-            await search.finish('请输入正确的序号!!!')
+        await search.finish('请输入正确的序号!!!')
     obindex = state['num_list'][int(num_input)]
     obname_full = state['name_list'][int(num_input)]
     pattern = r"\((.*?)\)[^()]*$"
